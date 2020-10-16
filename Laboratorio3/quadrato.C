50860b7eb3f4c1c7624f601aed885fd25884ae99
Dario Tamascelli <dario.tamascelli@unimi.it>
2020-10-16 11:48:41 +0200

    Update quadrato.C

--- Laboratorio3/quadrato.C
+++ Laboratorio3/quadrato.C
@@ -1,9 +1,14 @@
-
+#include <iostream>
+#include <cmath> //Contiente un sacco di funzioni interessanti, come pow (eleva a potenza), exp, sin, cos, tan,
 
 
 int a;
 int b;
+int c;
 
 a=5;
 
+
 b=a*a;
+
+c=pow(a,2);
