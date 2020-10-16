9304ac729b8ef46ac105b00fd3c88df6199e040b
Dario Tamascelli <dario.tamascelli@unimi.it>
2020-10-16 10:42:09 +0200

    Update euclideaPos.C

--- Laboratorio2/euclideaPos.C
+++ Laboratorio2/euclideaPos.C
@@ -13,14 +13,18 @@ int main(){
     cin >> a; //Legge da tastiera e registra valore in a
     cin >> b; //Legge da tastiera e registra valore in b
     
+    //cin >> a >> b;
+    
     //Dati acquisiti: ora procediamo
     
     resto=a;
+    
     quoz=0;
 
     if (a<0 or b<0){
         cout << endl <<"Corretto solo per dividendo e divisore entrambe positivi! Non faccio nulla ed Esco!" << endl;
     }
+    
     else if(b!=0){//Allora
 
         //Procediamo
